363146f153ec19cf36370971dbba7267ddfa4b43
Kha Cao <kha15cm@gmail.com>
2024-06-05 15:23:21 +0700

    feat: feadback request

---
+++ BusinessObjects/DTOs/Request/FeedBackRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.DTOs.Request
+{
+    public class FeedBackRequest
+    {
+        
+        public int CustomerId { get; set; }
+        public int TarotReaderId { get; set; }
+        public int? Rating { get; set; }
+        public string? Comments { get; set; }
+        
+    }
+}
