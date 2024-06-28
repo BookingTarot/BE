a7c0b2471a8e3792115b84eeca1524d088c8d30d
Kha Cao <kha15cm@gmail.com>
2024-06-28 11:54:19 +0700

    fix: Session type and tarot reader request

---
+++ BusinessObjects/DTOs/Request/SessionTypeRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.DTOs.Request
+{
+    public class SessionTypeRequest
+    {
+        public int SessionTypeId { get; set; }
+        public string? Name { get; set; }
+        public int? Duration { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
+        public bool? Status { get; set; }
+    }
+}
